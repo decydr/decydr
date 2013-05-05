9f56480fcddd5691dfd0303961bb26727d0e2f40
Michael Mitchell <michael.mitchell@g5searchmarketing.com>
2013-05-05 13:40:27 -0700

    adds new home page components etc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @events = Event.today
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :begin_time, :display_name, :end_time, :name, :web_link
-  scope :today, lambda { where("begin_time BETWEEN '#{DateTime.now.beginning_of_day}' AND '#{DateTime.now.end_of_day}'") }
+  attr_accessible :begin_time, :display_name, :end_time, :name, :web_link, :description
+  scope :today, lambda { where("begin_time BETWEEN '#{DateTime.now.beginning_of_day}' AND '#{DateTime.now.end_of_day}' OR
+                                end_time BETWEEN '#{DateTime.now.beginning_of_day}' AND '#{DateTime.now.end_of_day}'") }
 end
